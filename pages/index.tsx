af4c9784908ac48ab7ca4967d32ae6a58f43ac95
jiko797torayo <oec7dwayne7kj@gmail.com>
2021-07-05 00:45:22 +0900

    feat: ts導入

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,16 @@
-const Home = (props) => {
+import React, { FC } from "react";
+import { GetStaticProps } from "next";
+
+type Company = {
+  id: number;
+  name: string;
+}
+
+type Props = {
+  companies: Company[];
+}
+
+const Home: FC<Props> = (props) => {
   return (
     <div>
       <h2>POSTの一覧</h2>
@@ -14,7 +26,7 @@ const Home = (props) => {
   )
 }
 
-export const getStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async context => {
   const response = await fetch("http://api:3000/companies", {method: "GET"});
   const json = await response.json();
 
